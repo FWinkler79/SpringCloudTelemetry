1e4554d1ea687c02c7baf9eca5f39152ef681a3c
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2021-02-23 23:40:56 +0100

    Showing the use of baggage: - updated to latest Spring Boot and Spring Cloud Versions - added code to attach and retrieve baggage passed between services - added configurations to pass remote baggage - added configurations to include baggage into SLF4J MDC for log correlation - adjusted console log format and rsyslog format to include baggage from SLF4J MDC

--- sample-service-a/src/main/java/com/equalities/cloud/service/CustomSpanRestEndpoint.java
+++ sample-service-a/src/main/java/com/equalities/cloud/service/CustomSpanRestEndpoint.java
@@ -1,6 +1,7 @@
 package com.equalities.cloud.service;
 
-import org.springframework.cloud.sleuth.annotation.NewSpan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,9 @@ import com.equalities.cloud.service.persistence.model.Person;
 @RestController
 public class CustomSpanRestEndpoint {
   
+  @Autowired
+  private Tracer tracer;
+  
   private CustomSpanClient customSpanClient;
   
   CustomSpanRestEndpoint(CustomSpanClient customSpanClient) {
@@ -20,12 +24,19 @@ public class CustomSpanRestEndpoint {
   }
   
   @PostMapping("/custom-span")
-  @NewSpan("forwarding-person-from-a-to-b")
   public Person forward(@RequestBody Person person) {
-    
+//    BaggageField baggage = BaggageField.create("baggage-custom-baggage");
+//    baggage.updateValue("custom-value");
+//    
+//    BaggagePropagation.newFactoryBuilder(B3Propagation.FACTORY)
+//                      .add(SingleBaggageField
+//                      .remote(baggage))
+//                      .build();
     if (person.getLastName().endsWith("C")) {
       return person;
     }
+    
+    tracer.createBaggage("my-baggage-key", "my-baggage-value");
     
     person.setLastName(person.getLastName() + "-A");
     return customSpanClient.forward(person);
--- sample-service-c/src/main/java/com/equalities/cloud/service/CustomSpanRestEndpoint.java
+++ sample-service-c/src/main/java/com/equalities/cloud/service/CustomSpanRestEndpoint.java
@@ -1,17 +1,25 @@
 package com.equalities.cloud.service;
 
-import org.springframework.cloud.sleuth.annotation.NewSpan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.BaggageInScope;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.equalities.cloud.service.persistence.model.Person;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * A REST endpoint to show the use of custom child spans, trace context and support for baggage.
  */
+@Slf4j
 @RestController
 public class CustomSpanRestEndpoint {
+  
+  @Autowired
+  private Tracer tracer;
   
   private CustomSpanClient customSpanClient;
   
@@ -20,8 +28,12 @@ public class CustomSpanRestEndpoint {
   }
   
   @PostMapping("/custom-span")
-  @NewSpan("forwarding-person-from-c-to-a")
   public Person forward(@RequestBody Person person) {
+    BaggageInScope baggage = tracer.getBaggage("my-baggage-key");
+    String baggageValue = baggage.get();
+    
+    log.info("Baggage from span is: {}", baggageValue);
+    
     person.setLastName(person.getLastName() + "C");
     return customSpanClient.forward(person);
   }
